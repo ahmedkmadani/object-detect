5f863515267628995fce07dd5d438e8a86b0d6fe
ahmedkmadani <ahmedkamal543@gmail.com>
2020-01-13 12:23:10 +0000

    build github pages delpoy to git pg

---
+++ build/precache-manifest.ec4c072902bb657bf38d376088775467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290444af83ab36d68b337e58043902ab",
+    "url": "/object-detect/index.html"
+  },
+  {
+    "revision": "cd20b28036b4843e72b7",
+    "url": "/object-detect/static/css/main.68333960.chunk.css"
+  },
+  {
+    "revision": "fc3acad1ca32a4a3cee9",
+    "url": "/object-detect/static/js/2.3898fefe.chunk.js"
+  },
+  {
+    "revision": "b438f3216e6c3538fb8f06445de54096",
+    "url": "/object-detect/static/js/2.3898fefe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd20b28036b4843e72b7",
+    "url": "/object-detect/static/js/main.b4fdcf93.chunk.js"
+  },
+  {
+    "revision": "14b6fef105a5af58c87f",
+    "url": "/object-detect/static/js/runtime-main.a330a9d6.js"
+  }
+]);
